35536a95715065d28bea5a5d9688f3944d5e636c
Russell Heilling <russell@linx.net>
2016-07-06 11:51:17 +0100

    Fix some js bugs in 32p gatherer

--- gather/32p.js
+++ gather/32p.js
@@ -1,10 +1,10 @@
 (function() {
     'use strict';
 
-    var inkdrops = $(#stats_inkdrops .stat).text().trim()
+    var inkdrops = $("#stats_inkdrops .stat").text().trim();
     var stats = {
         'site': '32p',
-        'credits': inkdrops,
+        'credits': inkdrops
     };
     console.log(stats);
     chrome.runtime.sendMessage(stats);
