b5834e22980a6d09ca7aeccdac3353579e826a98
Russell Heilling <russell@linx.net>
2016-07-06 11:58:14 +0100

    post doens't handle missing metrics well.  add zero defaults to 32p gatherer

--- gather/32p.js
+++ gather/32p.js
@@ -4,6 +4,9 @@
     var inkdrops = $("#stats_inkdrops .stat").text().trim();
     var stats = {
         'site': '32p',
+        'up': 0,
+        'down': 0,
+        'ratio': 0,
         'credits': inkdrops
     };
     console.log(stats);
