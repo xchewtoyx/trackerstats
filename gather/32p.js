2e2428f64825d67d9207b2be2bb4dc3c801076b1
Russell Heilling <russell@linx.net>
2016-07-08 10:33:00 +0100

    Release 0.4.0  Support sparse stats.
    Support sites which do not have the full set of stats by
    checking for the presence first before adding the metrics to the
    post request.

--- gather/32p.js
+++ gather/32p.js
@@ -1,12 +1,13 @@
 (function() {
     'use strict';
 
-    var inkdrops = $("#stats_inkdrops .stat").text().trim();
+    var inkdrops =
+        $("#stats_inkdrops .stat").text()
+        .replace(',', '')
+        .trim();
+
     var stats = {
         'site': '32p',
-        'up': "0 Mb",
-        'down': "0 Mb",
-        'ratio': 0,
         'credits': inkdrops
     };
     console.log(stats);
--- post.js
+++ post.js
@@ -1,45 +1,63 @@
-'use strict';
-
-var FACTORS = {
-    'MB': 1000000,
-    'MiB': 1024 * 1024,
-    'GB': 1000000000,
-    'GiB': 1024 * 1024 * 1024,
-    'TB': 1000000000000,
-    'TiB': 1024 * 1024 * 1024 * 1024
-};
-
-function tobytes(valueString) {
-    var value = valueString.split(' ');
-    return value[0] * FACTORS[value[1]];
-}
-
-function postStats(stats, sender, sendResponse) {
-    var metrics = 'transfer_bytes,site=' + stats.site + ' up=';
-    metrics += tobytes(stats.up) + ',down=' + tobytes(stats.down) + '\n';
-    metrics += 'ratio,site=' + stats.site + ' value=' + stats.ratio + '\n';
-    if (stats.credits) {
-        metrics += 'credits,site=' + stats.site + ' value=' + stats.credits + '\n';
-    }
-    chrome.storage.sync.get(null, function (options) {
-      console.log(metrics);
-    
-      if(options.influxUrl && options.influxCredentials) {
-        $.ajax({
-          url: options.influxUrl,
-          type: 'POST',
-          data: metrics,
-          dataType: 'text',
-          headers: {
-            Authorization: 'Basic ' + options.influxCredentials,
-          }
-        });
-      } else {
-        console.log('Configure the Torrent Stats extension.');
-      }
-    });
-}
-
 (function() {
-    chrome.runtime.onMessage.addListener(postStats);
-})();
+    'use strict';
+
+    var FACTORS = {
+        'MB': 1000000,
+        'MiB': 1024 * 1024,
+        'GB': 1000000000,
+        'GiB': 1024 * 1024 * 1024,
+        'TB': 1000000000000,
+        'TiB': 1024 * 1024 * 1024 * 1024
+    };
+
+    function tobytes(valueString) {
+        var value = valueString.split(' ');
+        return value[0] * FACTORS[value[1]];
+    }
+
+    function postStats(stats, sender, sendResponse) {
+        var metrics = '';
+        console.log(new Date().toLocaleString() + ' site=' + stats.site);
+        if(stats.up && stats.down) {
+            metrics += 'transfer_bytes,site=' + stats.site;
+            metrics += ' up=' + tobytes(stats.up);
+            metrics += ',down=' + tobytes(stats.down) + '\n';
+        }
+        if(stats.ratio) {
+            metrics += 'ratio,site=' + stats.site;
+            metrics += ' value=' + stats.ratio + '\n';
+        }
+        if (stats.credits) {
+            metrics += 'credits,site=' + stats.site;
+            metrics += ' value=' + stats.credits + '\n';
+        }
+        chrome.storage.sync.get(null, function (options) {
+            console.log(metrics);
+            if(options.influxUrl && options.influxCredentials) {
+                $.ajax({
+                    url: options.influxUrl,
+                    type: 'POST',
+                    data: metrics,
+                    dataType: 'text',
+                    headers: {
+                        Authorization: 'Basic ' + options.influxCredentials,
+                    }
+                })
+                    .fail(function(jqXHR, textStatus) {
+                        console.log(
+                            'Failed to post stats to ' +
+                                options.influxUrl +
+                                ' Reason: ' +
+                                textStatus
+                        );
+                    });
+            } else {
+                console.log('Configure the Torrent Stats extension.');
+            }
+        });
+    }
+
+    (function() {
+        chrome.runtime.onMessage.addListener(postStats);
+    })();
+}());
